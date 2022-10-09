bf15dda853737eaa3963f1f60c125703bae04a3e
Udit <uditsharma1250@gmail.com>
2022-10-09 10:53:17 +0530

    form validation project completed

--- Project(form validation)/app.js
+++ Project(form validation)/app.js
@@ -5,26 +5,10 @@ const email = document.querySelector('.email');
 const phone = document.querySelector('.number');
 const car = document.querySelector('.car');
 
-let infoObj;
-let html = `
-<ul>
-    <li style="font-size:20px; font-weight:bold;">Name</li>
-    <li style="font-size:20px; font-weight:bold;">Car</li>
-    <li style="font-size:20px; font-weight:bold;">Contact</li>
-</ul>`;
-let info_display = document.querySelector('.info_display');
-let details = document.querySelector('.details');
-info_display.style.display = 'none';
-details.style.display = 'none';
-
-details.addEventListener('click',()=> {
-    document.querySelector('.info_submit').style.display = 'none';
-    info_display.style.display = 'block';
-});
 //'blur'meaning-> select and click outside
 let check_username; let check_email; let check_phone;
 username.addEventListener('blur', ()=> {
-    let regex = /^[a-zA-Z]([0-9a-zA-Z]){4,20}$/;      
+    let regex = /^[a-zA-Z]([0-9a-zA-Z]){3,20}$/;      
     let str = username.value;
     if(regex.test(str)) {
         username.classList.remove('is-invalid');
@@ -35,52 +19,67 @@ username.addEventListener('blur', ()=> {
         username.classList.add('is-invalid');
         document.querySelector('#list_name').innerHTML = `
         <li>Your username should not start with a number</li>
-        <li>Your username must be 4 to 10 character long</li>`;
+        <li>Your username must be 4 to 10 character long</li>
+        <li>Your username should not contain any special characters or spaces`;
         check_username = false;
     }
-    setTimeout(()=> username.classList.remove('is-valid'),2000);
 });
+
+
 email.addEventListener('blur', ()=> {
-    check_email = true;
+    let regex = /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/;      
+    let str = email.value;
+    if(regex.test(str)) {
+        email.classList.remove('is-invalid');
+        email.classList.add('is-valid');
+        check_email = true;
+    }    
+    else {
+        email.classList.add('is-invalid');
+        document.querySelector('#list_email').innerHTML = `
+        <li>Please add an valid email</li>`;
+        check_email = false;
+    }
 });
+
+
 phone.addEventListener('blur', ()=> {
-    let regex = /([0-9]){10}/;
+    let regex = /^([0-9]){10}$/;
     let number = phone.value;
     if(regex.test(number)) {
-        phone.classList.remove('is-invalid');
         phone.classList.add('is-valid');
+        phone.classList.remove('is-invalid');
         check_phone = true;
     }
     else {
         phone.classList.add('is-invalid');
+        document.querySelector('#list_phone').innerHTML = `
+        <li>Your phone number should be 10-digits long</li>
+        <li>Your number should not contain any aplhabets or symbols</li>`;
         check_phone = false;
     }
-    setTimeout(()=> {
-        phone.classList.remove('is-valid');
-        phone.classList.remove('is-invalid');
-    },2000);
 });
+
 
 document.querySelector('.submit').addEventListener('click', ()=> {
 
     let message = document.querySelector('.successmessage');
-    if(check_email==true && check_phone==true && check_username==true){
-        details.style.display = 'block';
+    if(check_email && check_phone && check_username){
         message.innerHTML = `
         <div class="alert alert-success alert-dismissible fade show" role="alert">
             <div style="text-align: center;">
                 <strong>Success!</strong> You have successfully submitted the from
             </div>
         </div>`;
-        html += `<ul>
-        <li>${username.value}</li>
-        <li>${car.value}</li>
-        <li>${phone.value}</li>
-        </ul>`;
-        info_display.innerHTML = html;
+        let obj = {name: username.value, email: email.value, phone: phone.value};
+        console.log(obj);
         username.value = "";
         email.value = "";
         phone.value = "";
+        check_email = check_phone = check_username = false;
+        username.classList.remove('is-valid');
+        email.classList.remove('is-valid');
+        phone.classList.remove('is-valid');
         setTimeout(()=>{
             message.innerHTML = "";
         }, 2500);
